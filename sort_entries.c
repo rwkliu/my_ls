e312c7317aec16aa148e89563339abe2765a6c2b
Ricky <ricky.liu.994@gmail.com>
2022-10-20 00:31:30 -0700

    sort_entries.c count_entries locally opens and closes the directory count_entries input parameter changed to directory name string

--- sort_entries.c
+++ sort_entries.c
@@ -13,13 +13,17 @@ typedef struct s_dirent_array {
 } dirent_array;
 #endif
 
-int count_entries(DIR *directory) {
+int count_entries(char *dir_name) {
+  DIR *directory;
   int entries = 0;
   struct dirent *dir_entry;
+
+  directory = opendir(dir_name);
 
   while(dir_entry = readdir(directory)) {
     entries++;
   }
+  closedir(directory);
   return entries;
 }
 
@@ -28,18 +32,19 @@ int main() {
   struct dirent *entry;
   struct stat filestat;
   dirent_array entry_array;
-  int entries = 0;
+  int index = 0;
+  char *directory_name = ".";
 
-  folder = opendir(".");
+  entry_array.size = count_entries(directory_name);
+  folder = opendir(directory_name);
   entry = readdir(folder);
 
-  entry_array.size = count_entries(folder);
-  entry_array.array = malloc(entry_array.size * sizeof(DIR *));
-  entry_array.array[0] = entry;
+  entry_array.array = malloc(entry_array.size * sizeof(struct dirent *));
 
   //Read directory entries and assign dirent_array->array point to the entries
   printf("Entry 1: %s\n", entry_array.array[0]->d_name);
   
   free(entry_array.array);
+  closedir(folder);
   return 0;
 }
