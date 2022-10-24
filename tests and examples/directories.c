67a98fdad34e06a8f0e209de0602770daece13ff
Ricky <ricky.liu.994@gmail.com>
2022-10-23 17:39:26 -0700

    directories.c append entry name to directory handle inside opendir() pass in appended file path name to stat()

--- tests and examples/directories.c
+++ tests and examples/directories.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <time.h>
-#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
 
 int main() {
   DIR *folder = opendir("/home");  //directory handle for the opendir function
   struct dirent *entry; //contains directory entry information
   struct stat filestat; //contains info about a directory entry
   int files = 0;
+  char *path = strdup("/home/");
+  int input_path_len = strlen(path);
 
   while((entry = readdir(folder)) != NULL) {
     files++;
-    if(stat(entry->d_name, &filestat) == -1) {
+    path = realloc(path, strlen(path) + strlen(entry->d_name)* sizeof(char));
+    strcat(path, entry->d_name);
+    int stat_result = stat(path, &filestat);
+    if(stat_result == -1) {
+      printf("Errno: %d\n", errno);
       printf("Unable to get file properties\n");
     }
     else {
       printf("%d: %s %s", files, entry->d_name, ctime(&filestat.st_mtim.tv_sec));
     }
+    path[input_path_len] = '\0';
   }
 
   closedir(folder);
