6e94d3b75c31b49428baf2d8b4e7b225b06e4b13
Ricky <ricky.liu.994@gmail.com>
2022-10-23 18:24:13 -0700

    directories.c Check if file_path is "."

--- tests and examples/directories.c
+++ tests and examples/directories.c
@@ -8,32 +8,34 @@
 #include <string.h>
 
 int main() {
-  char *file_path = "/home";
+  char *file_path = "/home/";
   DIR *folder = opendir(file_path);  //directory handle for the opendir function
   struct dirent *entry; //contains directory entry information
   struct stat filestat; //contains info about a directory entry
   int files = 0;
+  int stat_result;
   char *path = strdup(file_path);
   int file_path_len = strlen(path);
 
-  if(file_path[strlen(file_path)] != '/') {
-    path = realloc(path, strlen(path) + 1 * sizeof(char));
-    strcat(path, "/");
-    file_path_len = strlen(path);
+  if(strcmp(file_path, ".") != 0) {
+    if(file_path[strlen(file_path)] != '/') {
+      path = realloc(path, strlen(path) + 1 * sizeof(char));
+      strcat(path, "/");
+      file_path_len = strlen(path);
+    }
   }
-  // if(strcmp(file_path, ".") != 0) {
-
-  // }
-  // else {
-  //   file_path = ".";
-  // }
-
 
   while((entry = readdir(folder)) != NULL) {
     files++;
-    path = realloc(path, strlen(path) + strlen(entry->d_name) * sizeof(char));
-    strcat(path, entry->d_name);
-    int stat_result = stat(path, &filestat);
+    if(strcmp(file_path, ".") != 0) {
+      path = realloc(path, strlen(path) + strlen(entry->d_name) * sizeof(char));
+      strcat(path, entry->d_name);
+      stat_result = stat(path, &filestat);
+    }
+    else {
+      stat_result = stat(path, &filestat);
+    }
+    
     if(stat_result == -1) {
       printf("Errno: %d\n", errno);
       printf("Unable to get file properties\n");
